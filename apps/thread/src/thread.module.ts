5380a60200bcfdf479b533f9622c4cf0d7d8cf5a
Võ Tấn Đạt <91039754+VNKaiSer@users.noreply.github.com>
2024-01-17 16:43:30 +0700

    add socket.io

--- apps/thread/src/thread.module.ts
+++ apps/thread/src/thread.module.ts
@@ -3,10 +3,11 @@ import { ThreadController } from './thread.controller';
 import { ThreadService } from './thread.service';
 import { ThreadRepository } from './thread.repository';
 import { CommonModule, PrismaModule } from '@app/common';
+import { AppGateway } from './app.gateway';
 
 @Module({
   imports: [CommonModule, PrismaModule],
   controllers: [ThreadController],
-  providers: [ThreadService, ThreadRepository],
+  providers: [ThreadService, ThreadRepository, AppGateway],
 })
 export class ThreadModule {}
